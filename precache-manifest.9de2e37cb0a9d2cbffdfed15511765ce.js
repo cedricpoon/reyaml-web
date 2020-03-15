03ac821dad9d61e16080993778fb743bc033cca4
Cedric Poon <cedric@poons.me>
2020-03-16 01:15:17 +0800

    Updates

---
+++ precache-manifest.9de2e37cb0a9d2cbffdfed15511765ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81923f314e1fd81c2b15cff7f8cd0727",
+    "url": "/reyaml-web/index.html"
+  },
+  {
+    "revision": "cdf37dd7a2e1e6d24830",
+    "url": "/reyaml-web/static/css/2.a453beb8.chunk.css"
+  },
+  {
+    "revision": "c7eb741e4e3ba8bbd412",
+    "url": "/reyaml-web/static/css/main.dd146d06.chunk.css"
+  },
+  {
+    "revision": "cdf37dd7a2e1e6d24830",
+    "url": "/reyaml-web/static/js/2.e54b619e.chunk.js"
+  },
+  {
+    "revision": "6358a22d0936d336b0a91b174c46c62e",
+    "url": "/reyaml-web/static/js/2.e54b619e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7eb741e4e3ba8bbd412",
+    "url": "/reyaml-web/static/js/main.3b0f7d38.chunk.js"
+  },
+  {
+    "revision": "5aecef29722785a1a454",
+    "url": "/reyaml-web/static/js/runtime-main.6a1ff2db.js"
+  }
+]);
