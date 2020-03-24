5e6aa42e1efc2fcc7b09921a238af229c304cf46
Cedric Poon <cedric@poons.me>
2020-03-25 02:09:21 +0800

    v0.3.0 (#4)
    * d3Object > redux: state.d3.object
    
    * revamp structure of toolbar
    
    * structure store.context
    
    * New file
    
    * Update MenuItem.js
    
    change click outside dropdown close behaviour
    
    * complete open file
    
    * todo: state.context.row -> state.context.selection
    
    * refactor defaultState with state.cursor
    
    * toast and more
    
    * Update Workbench.js
    
    init scroll bugfix via `resize(true)`
    
    * complete saveas
    
    * v0.2.0
    
    * complete File
    
    * v0.3.0

--- src/hoc/Toolbar/SubMenus/File.js
+++ src/hoc/Toolbar/SubMenus/File.js
@@ -8,7 +8,35 @@ import toast from 'toast';
 import { resetContext, updateContext } from 'actions';
 import { persistor } from 'App';
 
+const svgConfig = {
+  container: [ "svg", "g" ],
+  styles: {
+    "rect": [ "fill", "stroke", "stroke-width" ],
+    "path": [ "fill", "stroke", "stroke-width" ],
+    "circle": [ "fill", "stroke", "stroke-width" ],
+    "line": [ "stroke", "stroke-width" ],
+    "text": [ "fill", "font-size", "text-anchor", "font" ],
+    "polygon": [ "stroke", "fill" ]
+  }
+}
+
 class File extends React.Component {
+  _exportSvgStyle = (cloned, source) => {
+    cloned.childNodes.forEach((clonedChild, i) => {
+      if (svgConfig.container.indexOf(clonedChild.tagName) !== -1)
+        this._exportSvgStyle(clonedChild, source.childNodes[i])
+
+      else if (clonedChild.tagName in svgConfig.styles) {
+        const styleDef = window.getComputedStyle(source.childNodes[i]);
+        let styleDefStr = "";
+        svgConfig.styles[clonedChild.tagName].forEach(style => {
+          styleDefStr += `${style}:${styleDef.getPropertyValue(style)}; `
+        });
+        clonedChild.setAttribute("style", styleDefStr);
+      }
+    });
+  }
+
   _openYaml = e => {
     if (this.fileOpen.files[0]) {
       const reader = new FileReader();
@@ -20,6 +48,35 @@ class File extends React.Component {
       reader.onerror = e => {
         toast(`${this.props.t('alert-file-error')} "${this.fileOpen.files[0].name}"`);
       }
+    }
+  }
+
+  _saveYaml = e => {
+    this.saveAs.download = window.prompt(this.props.t('alert-saveas-filename'), this.saveAs.download) || '';
+    if (this.saveAs.download.length > 0) {
+      this.saveAs.click();
+      toast(`${this.props.t("alert-saveas-done")} "${this.saveAs.download}"`);
+    }
+  }
+
+  _exportSvg = e => {
+    try {
+      const svg = document
+        .getElementsByClassName('rd3t-tree-container rd3t-grabbable')[0]
+        .children[0];
+      const clonedSvg = svg.cloneNode(true);
+
+      this._exportSvgStyle(clonedSvg, svg);
+      this.export.href = `data:image/svg+xml,${encodeURIComponent(new XMLSerializer().serializeToString(clonedSvg))}`;
+
+      this.export.download = window.prompt(this.props.t('alert-saveas-filename'), this.export.download) || '';
+      if (this.export.download.length > 0) {
+        this.export.click();
+        toast(`${this.props.t("alert-saveas-done")} "${this.export.download}"`);
+      }
+    } catch (e) {
+      toast(this.props.t('alert-export-err'));
+      console.warn(e);
     }
   }
 
@@ -39,30 +96,30 @@ class File extends React.Component {
           {t('toolbar-file-open')}
         </MenuItem>
         <DropdownMenu.LineBreak />
-        <MenuItem button onClick={e => { 
+        <MenuItem button onClick={e => {
           persistor.flush();
           toast(this.props.t('alert-save-done'));
         }}>
           {t('toolbar-file-save')}
         </MenuItem>
-        <MenuItem button onClick={e => {
-          this.saveAs.download = window.prompt(this.props.t('alert-saveas-filename'), this.saveAs.download) || '';
-          if (this.saveAs.download.length > 0) {
-            this.saveAs.click();
-            toast(`${this.props.t("alert-saveas-done")} "${this.saveAs.download}"`);
-          }
-        }}>
+        <MenuItem button onClick={this._saveYaml}>
           {t('toolbar-file-saveas')}
         </MenuItem>
         <DropdownMenu.LineBreak />
-        <MenuItem button>{t('toolbar-file-export-link')}</MenuItem>
-        <MenuItem button>{t('toolbar-file-export-diagram')}</MenuItem>
+        <MenuItem button onClick={this._exportSvg}>
+          {t('toolbar-file-export-diagram')}
+        </MenuItem>
         <section hidden>
           <input type='file' accept='.yaml,.yml' ref={r => this.fileOpen = r} onChange={this._openYaml} />
-          <a 
+          <a
             ref={r => this.saveAs = r}
             href={`data:application/x-yaml;charset=utf-8,${encodeURIComponent(this.props.yaml)}`}
             download='download.yml'
+          >-</a>
+          <a
+            ref={r => this.export = r}
+            href='about:blank'
+            download='yaml.svg'
           >-</a>
         </section>
       </DropdownMenu>
